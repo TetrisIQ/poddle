c828498475a69276158f971f68f85229b19c3111
alwerner <alexander.werner@bonprix.net>
2022-02-08 21:29:34 +0100

    fix: it's too late

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: process.env.NODE_ENV === "development" ? '' : '/polldapp/'
+  base: process.env.NODE_ENV === "development" ? '' : '/poll-dapp/'
 })
