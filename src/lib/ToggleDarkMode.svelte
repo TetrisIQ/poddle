8694f3956b109a20b12c0416e342f857a7103118
PIYUSH NEGI <43876655+npiyush97@users.noreply.github.com>
2022-02-18 00:07:51 +0530

    Update ToggleDarkMode.svelte
    Light mode / Dark mode doesn't get saved #6
    
    fix / cleaning required

--- src/lib/ToggleDarkMode.svelte
+++ src/lib/ToggleDarkMode.svelte
@@ -1,21 +1,35 @@
 <script lang="ts">
     let darkMode = true;
 
-    function changeMode() {
+    (function setMode(){
+        if(localStorage.getItem('mode') == 'dark'){
+            document.querySelector('html').classList.add(localStorage.getItem('mode'))
+            darkMode = false
+        }
+        else{
+            darkMode = true
+        }
+    })()
+
+    
+       function changeMode() {
         if(darkMode) {
             document.querySelector("html").classList.add('dark');
+            localStorage.setItem('mode','dark')
         } else {
             document.querySelector("html").classList.remove('dark');
+            localStorage.setItem('mode','')
         }
+        console.log(darkMode,localStorage.getItem('mode'))
         darkMode = !darkMode;
-    }
+    }}
 
 </script>
 
 
 <!-- Not toggled switch -->
 {#if darkMode}
-    <div on:click={() => changeMode()} class="my-auto mx-4 flex justify-center items-center">
+    <div on:click={() => changeMode()} class="my-auto mx-4 flex justify-center items-center cursor-pointer my-4">
         <svg class="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                   d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"/>
@@ -34,7 +48,7 @@
     </div>
 {:else }
     <!-- Toggled switch -->
-    <div on:click={() => changeMode()} class="my-auto mx-4 flex justify-center items-center mt-4">
+    <div on:click={() => changeMode()} class="my-auto mx-4 flex justify-center items-center cursor-pointer my-4">
     <span class="">
         <svg class="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
