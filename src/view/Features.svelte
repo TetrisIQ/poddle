f2a91a72eeea71bfd0e654b81efba832bb1a76f4
alwerner <alexander.werner@bonprix.net>
2022-02-19 15:16:24 +0100

    feat: adding poll counter and Statistics on Feature page

--- src/view/Features.svelte
+++ src/view/Features.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
+    import {gun} from "../gun";
+    import dayjs from "dayjs";
+    import RelativeTime from "dayjs/plugin/relativeTime"
 
+    dayjs.extend(RelativeTime)
+
+    let pollCounter = -1;
+    let gitStars = -1;
+    let lastUpdate = dayjs();
+    gun.getPollCounter().then(res => pollCounter = res)
+
+    // Fetch github API
+    fetch("https://api.github.com/repos/tetrisiq/poll-dapp", {method: 'GET', redirect: 'follow'})
+        .then(response => response.json())
+        .then(result => {
+            gitStars = result.stargazers_count;
+            lastUpdate = dayjs(result.pushed_at);
+        })
+        .catch(error => console.log('error', error));
 
 </script>
 
@@ -126,6 +144,60 @@
                     </a>-->
                 </div>
             </div>
+        </div>
+    </div>
+</section>
+
+<!-- Statistics -->
+<section class="dark:text-gray-400 dark:bg-gray-900 text-gray-600 body-font">
+    <div class="container px-5 py-8 mx-auto">
+        <div class="flex flex-col text-center w-full mb-5">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 dark:text-white">Statistics</h1>
+        </div>
+        <div class="flex flex-wrap -m-3 text-center justify-center">
+            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                <div class="border-2 dark:border-gray-800 border-gray-200 px-4 py-6 rounded-lg"
+                     on:click={() => window.open("https://github.com/tetrisiq/poll-dapp","_blank")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                         class="dark:text-indigo-400 text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 16 16">
+                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                    </svg>
+                    <h2 class="title-font font-medium text-3xl dark:text-white text-gray-900">{gitStars}</h2>
+                    <p class="leading-relaxed">GitHub Stars</p>
+                </div>
+            </div>
+            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                <div class="border-2 dark:border-gray-800 border-gray-200 px-4 py-6 rounded-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                         class="dark:text-indigo-400 text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                              d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3.854 2.146a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 3.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 7.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
+                    </svg>
+                    <h2 class="title-font font-medium text-3xl dark:text-white ext-gray-900">~ {pollCounter}</h2>
+                    <p class="leading-relaxed">Created Polls</p>
+                </div>
+            </div>
+            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                <div class="border-2 dark:border-gray-800 border-gray-200 px-4 py-6 rounded-lg"
+                     on:click={() => window.open("https://github.com/tetrisiq/poll-dapp","_blank")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                         class="dark:text-indigo-400 text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                        <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+                    </svg>
+                    <h2 class="title-font font-medium text-3xl dark:text-white text-gray-900">{lastUpdate.fromNow()}</h2>
+                    <p class="leading-relaxed">Last update</p>
+                </div>
+            </div>
+            <!-- <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                 <div class="border-2 dark:border-gray-800 border-gray-200 px-4 py-6 rounded-lg">
+                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="dark:text-indigo-400 text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+                         <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                     </svg>
+                     <h2 class="title-font font-medium text-3xl dark:text-white text-gray-900">46</h2>
+                     <p class="leading-relaxed">Places</p>
+                 </div>
+             </div>-->
         </div>
     </div>
 </section>
