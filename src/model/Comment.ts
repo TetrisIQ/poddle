1bf03235db2e3d5ab1aba43d729c38811f1f1b76
alwerner <alexander.werner@bonprix.net>
2022-02-20 15:23:36 +0100

    feat: restyle the poll page
    Some restyling after user-feedback

--- src/model/Comment.ts
+++ src/model/Comment.ts
@@ -3,20 +3,20 @@ export class Comment {
     comment: string;
     time: Date;
 
-    constructor(name: string, comment: string, time: Date) {
-        if (name === undefined) {
-            this.name = "Anonymous"
-        } else {
-            this.name = name;
-        }
+    constructor(name: string, comment: string = "", time: Date = new Date()) {
+        this.name = name;
         this.comment = comment;
         this.time = time;
     }
 
     getPrintableDate(): string {
-        if(this.time === undefined) {
+        if (this.time === undefined) {
             return "Invalid Time"
         }
         return this.time.toLocaleDateString();
+    }
+
+    updateTime() {
+        this.time = new Date();
     }
 }
