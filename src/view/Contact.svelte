82f73fa8c329ef80d590d3034d5624772e589b77
alwerner <alexander.werner@bonprix.net>
2022-02-26 17:33:47 +0100

    fix: add netlify attribute

--- src/view/Contact.svelte
+++ src/view/Contact.svelte
@@ -1,4 +1,4 @@
-<form data-netlify="true" class="dark:text-gray-400 dark:bg-gray-900 text-gray-600 body-font relative">
+<form netlify data-netlify="true" class="dark:text-gray-400 dark:bg-gray-900 text-gray-600 body-font relative">
     <div class="container px-5 py-24 mx-auto">
         <div class="flex flex-col text-center w-full mb-12">
             <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 dark:text-white text-gray-900">Contact Us</h1>
