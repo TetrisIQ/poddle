a9fd4ce940dbd8e7f5afdc62ad430f74452f5760
alwerner <alexander.werner@bonprix.net>
2022-02-26 17:46:31 +0100

    fix: test

--- src/view/Contact.svelte
+++ src/view/Contact.svelte
@@ -30,6 +30,11 @@
                     </div>
                 </div>
                 <div class="p-2 w-full">
+                    <div  class="relative">
+                        <div data-netlify-recaptcha></div>
+                    </div>
+                </div>
+                <div class="p-2 w-full">
                     <button type="submit" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                         Button
                     </button>
