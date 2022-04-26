ff98b0b48058304916941e0587836266d2128155
alwerner <alexander.werner@bonprix.net>
2022-04-26 16:24:27 +0200

    fix: remove dark mode :(

--- frontend/src/lib/Settings.svelte
+++ frontend/src/lib/Settings.svelte
@@ -59,15 +59,16 @@
 </script>
 
 <br>
-<h1 class="text-2xl">Settings</h1>
-<div class="mt-6 grid grid-flow-col justify-between auto-cols-max">
+<h1 class="text-2xl dark:text-white">Settings</h1>
+<!-- Dark mode is removed for now -->
+<!--<div class="mt-6 grid grid-flow-col justify-between auto-cols-max dark:text-white">
     <p class="text-xl py-4">Toggle dark mode</p>
     <ToggleDarkMode/>
-</div>
+</div>-->
 
-<p class="text-xl mb-2 mt-6">Change your relay servers</p>
+<p class="text-xl mb-2 mt-6 dark:text-white">Change your relay servers</p>
 <table class="w-full">
-    <tr class="w-1/2 bg-indigo-300">
+    <tr class="w-1/2 dark:bg-indigo-700 dark:text-white bg-indigo-300">
         <th class="p-2 w-2/3">Relay Name</th>
         <th class="p-2 text-center">Status</th>
         <th class="p-2 text-center pr-4">Country</th>
@@ -75,7 +76,7 @@
     </tr>
     {#if $allRelays.length > 0}
         {#each $allRelays as relay , id}
-            <tr class="{id % 2 === 0} bg-gray-100">
+            <tr class="{id % 2 === 0} bg-gray-100 dark:bg-gray-500 dark:text-white">
                 <td class="p-2">{relay.name}</td>
                 <td class="p-2 text-center {relay.status === 'UP' ? 'text-green-600' : 'text-red-600'}">{relay.status}</td>
                 <td class="p-2 pr-4 flex justify-between"><span class="incident text-red-500"><!-- TODO: Display Warnings --></span><span
@@ -102,7 +103,7 @@
 </table>
 <div class="flex py-4">
     <button on:click={() => updateRelayServer()}
-            class="inline-flex items-center dark:bg-gray-800 bg-gray-100 border-0 py-1 px-3 focus:outline-none dark:hover:bg-gray-700 hover:bg-gray-200 rounded text-base ml-auto mt-4 md:mt-0">
+            class="inline-flex dark:text-white items-center dark:bg-gray-800 bg-gray-100 border-0 py-1 px-3 focus:outline-none dark:hover:bg-gray-700 hover:bg-gray-200 rounded text-base ml-auto mt-4 md:mt-0">
         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
              class="mx-2 w-4 h-4 ml-1" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
@@ -113,7 +114,7 @@
 </div>
 
 <div class="mt-6 grid grid-flow-col justify-between auto-cols-max">
-    <p class="text-xl py-4">Delete browser data</p>
+    <p class="text-xl dark:text-white py-4">Delete browser data</p>
     <button on:click={() => deleteAllBrowserData()}
             class="text-white h-min my-auto bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
         Delete
