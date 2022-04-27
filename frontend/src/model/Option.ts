29184e6447ae662cbad42e1a19b4baca176e3b71
alwerner <alexander.werner@bonprix.net>
2022-04-27 20:00:55 +0200

    fix: :sparkles: Option counter
    Fixing the option counter in Poll View

--- frontend/src/model/Option.ts
+++ frontend/src/model/Option.ts
@@ -1,14 +1,15 @@
 export class Option {
-    id: number;
-    option: string;
-    type: string;
-    count: number;
+  id: number;
+  option: string;
+  type: string;
+  count: number;
+  ifNeeded: number;
 
-    constructor(id: number = 0, option: string = "", type: string = "text") {
-        this.id = id;
-        this.option = option;
-        this.count = 0;
-        this.type = type
-    }
-
+  constructor(id: number = 0, option: string = "", type: string = "text") {
+    this.id = id;
+    this.option = option;
+    this.count = 0;
+    this.type = type;
+    this.ifNeeded = 0;
+  }
 }
