691e77b1889ab0c5b0a5465809abe2232085b306
Alex <dev@tetrisiq.de>
2022-05-05 10:39:08 +0200

    feat: :iphone: create Poll on mobile
    Change the design of creating a poll and optimise for mobile

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,65 +1,68 @@
 <script lang="ts">
-    import Header from "./lib/Header.svelte";
-    import {page, releaseMessage} from "./store";
-    import Poll from './view/Poll.svelte'
-    import Footer from "./lib/Footer.svelte";
-    import NotificationArea from "./lib/NotificationArea.svelte";
-    import {gun} from "./gun";
-    import NewMessage from "./view/NewMessage.svelte";
-    import dayjs from "dayjs";
-    import {onMount} from "svelte";
-    import Modal from "./lib/Modal.svelte";
+  import Header from './lib/Header.svelte';
+  import { page, releaseMessage } from './store';
+  import Poll from './view/Poll.svelte';
+  import Footer from './lib/Footer.svelte';
+  import NotificationArea from './lib/NotificationArea.svelte';
+  import { gun } from './gun';
+  import NewMessage from './view/NewMessage.svelte';
+  import dayjs from 'dayjs';
+  import Modal from './lib/Modal.svelte';
 
-    if (window.location.search.includes("?k=")) {
-        page.set(Poll)
+  if (window.location.search.includes('?k=')) {
+    page.set(Poll);
+  }
+
+  if (window.location.search.includes('?newMessage')) {
+    page.set(NewMessage);
+  }
+
+  gun.getReleaseMessage();
+
+  function ttlIsOver() {
+    if ($releaseMessage !== undefined) {
+      const ttlInDays = +$releaseMessage.ttl;
+      return dayjs().isAfter(dayjs($releaseMessage.createdOn).add(ttlInDays, 'day').endOf('day').toISOString());
     }
-
-    if (window.location.search.includes("?newMessage")) {
-        page.set(NewMessage)
-    }
-
-    gun.getReleaseMessage()
-
-    function ttlIsOver() {
-        if ($releaseMessage !== undefined) {
-            const ttlInDays = +$releaseMessage.ttl;
-            return dayjs().isAfter(dayjs($releaseMessage.createdOn).add(ttlInDays, 'day').endOf('day').toISOString())
-        }
-    }
-
-
+  }
 </script>
-<div class="flex flex-col min-h-screen">
-    <Header/>
-    {#if $releaseMessage !== undefined && !ttlIsOver()}
-        {#if $releaseMessage.kind === "release"}
-            <div on:click={() => releaseMessage.set(undefined)}
-                 class="w-full h-auto dark:text-black mx-auto text-center h-9 bg-indigo-400 p-1 text-lg">
-                <span>📦</span>
-                <span class="font-bold pr-2">{$releaseMessage.title}</span>
-                <span>{$releaseMessage.content}</span>
-            </div>
-        {:else if ($releaseMessage.kind === "celebrate")}
-            <div on:click={() => releaseMessage.set(undefined)}
-                 class="w-full dark:text-black h-auto mx-auto text-center h-9 bg-indigo-400 p-1 text-lg">
-                <span>🎉</span>
-                <span class="font-bold pr-2">{$releaseMessage.title}</span>
-                <span>{$releaseMessage.content}</span>
-            </div>
-        {:else if ($releaseMessage.kind === "warning")}
-            <div on:click={() => releaseMessage.set(undefined)}
-                 class="w-full dark:bg-gray-500 dark:text-black h-auto mx-auto text-center h-9 bg-gray-200 p-1 text-lg">
-                <span>👷</span>
-                <span class="font-bold pr-2">{$releaseMessage.title}</span>
-                <span>{$releaseMessage.content}</span>
-            </div>
-        {/if}
-    {/if}
-    <main class="mx-auto flex-grow w-full xl:px-40 l:px-14 text-center">
-        <NotificationArea/>
-        <Modal/>
-        <svelte:component this={$page}/>
-    </main>
-    <Footer/>
-</div>
 
+<div class="flex flex-col min-h-screen scroll-smooth">
+  <Header />
+  {#if $releaseMessage !== undefined && !ttlIsOver()}
+    {#if $releaseMessage.kind === 'release'}
+      <div
+        on:click={() => releaseMessage.set(undefined)}
+        class="w-full h-auto dark:text-black mx-auto text-center h-9 bg-indigo-400 p-1 text-lg"
+      >
+        <span>📦</span>
+        <span class="font-bold pr-2">{$releaseMessage.title}</span>
+        <span>{$releaseMessage.content}</span>
+      </div>
+    {:else if $releaseMessage.kind === 'celebrate'}
+      <div
+        on:click={() => releaseMessage.set(undefined)}
+        class="w-full dark:text-black h-auto mx-auto text-center h-9 bg-indigo-400 p-1 text-lg"
+      >
+        <span>🎉</span>
+        <span class="font-bold pr-2">{$releaseMessage.title}</span>
+        <span>{$releaseMessage.content}</span>
+      </div>
+    {:else if $releaseMessage.kind === 'warning'}
+      <div
+        on:click={() => releaseMessage.set(undefined)}
+        class="w-full dark:bg-gray-500 dark:text-black h-auto mx-auto text-center h-9 bg-gray-200 p-1 text-lg"
+      >
+        <span>👷</span>
+        <span class="font-bold pr-2">{$releaseMessage.title}</span>
+        <span>{$releaseMessage.content}</span>
+      </div>
+    {/if}
+  {/if}
+  <main class="mx-auto flex-grow w-full xl:px-40 l:px-14 text-center">
+    <NotificationArea />
+    <Modal />
+    <svelte:component this={$page} />
+  </main>
+  <Footer />
+</div>
--- frontend/src/view/CreatePoll/CreatePoll3.svelte
+++ frontend/src/view/CreatePoll/CreatePoll3.svelte
@@ -1,93 +1,99 @@
 <script lang="ts">
-    import {page, currentPoll} from "../../store";
-    import Checkbox from "../../lib/Checkbox.svelte";
-    import CreatePoll4 from "./CreatePoll4.svelte";
-    import DatePicker from "@beyonk/svelte-datepicker/src/components/DatePicker.svelte";
-    import dayjs from "dayjs";
-    import {Settings} from "../../model/PollSettings";
+  import { page, currentPoll } from '../../store';
+  import Checkbox from '../../lib/Checkbox.svelte';
+  import CreatePoll4 from './CreatePoll4.svelte';
+  import DatePicker from '@beyonk/svelte-datepicker/src/components/DatePicker.svelte';
+  import dayjs from 'dayjs';
+  import { Settings } from '../../model/PollSettings';
 
-    //create local Settings
-    let settings : Settings = new Settings();
+  //create local Settings
+  let settings: Settings = new Settings();
 
-    function next() {
-        $currentPoll.settings = settings;
-        page.set(CreatePoll4);
-    }
-
+  function next() {
+    $currentPoll.settings = settings;
+    page.set(CreatePoll4);
+  }
 </script>
 
+<form on:submit|preventDefault={() => next()} class="mx-auto max-w-4xl rounded overflow-hidden min-h-screen md:min-h-0">
+  <div class="font-bold text-xl mb-2">Poll settings</div>
+  <div class="flex my-2">
+    <Checkbox bind:checked={settings.treeOptions} class="ml-2 mb-auto" size="2rem" primaryColor="#f7be00" />
+    <div>
+      <div class="pl-2">
+        <p class="font-bold text-left">Yes, no, if need be</p>
+        <p class="font-light text-left">Participants can indicate if an option is not ideal for them.</p>
+      </div>
+    </div>
+  </div>
 
-<form on:submit|preventDefault={() => next()}
-      class="mx-auto max-w-4xl rounded overflow-hidden shadow-lg">
-    <!--    <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">-->
-    <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">Poll settings</div>
-        <div class="flex-wrap text-left">
-            <div class="grid justify-end grid-cols-3 gap-4 py-4">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
-                     class="ml-auto mr-5 my-auto" viewBox="0 0 16 16">
-                    <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
-                    <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
-                </svg>
-                <div>
-                    <h2 class="font-bold">Yes, no, if need be</h2>
-                    <p>Participants can indicate if an option is not ideal for them.</p>
-                </div>
-                <Checkbox bind:checked={settings.treeOptions}
-                          primaryColor="#f7be00" class="my-auto ml-5"/>
-            </div>
-            <div class="grid justify-end grid-cols-3 gap-4 py-4">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
-                     class="ml-auto mr-5 my-auto" viewBox="0 0 16 16">
-                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/>
-                </svg>
-                <div>
-                    <h2 class="font-bold">Limit the number of votes per option</h2>
-                    <p>First come, first served. Once the spots are filled, the option is no longer available.</p>
-                </div>
-                <Checkbox disabled={settings.treeOptions} bind:checked={settings.fcfs}
-                          primaryColor="#f7be00" class="my-auto ml-5"/>
-            </div>
-            <div class="grid justify-end grid-cols-3 gap-4 py-4">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
-                     class="ml-auto mr-5 my-auto" viewBox="0 0 16 16">
-                    <circle cx="8" cy="8" r="1.5"/>
-                    <path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h10zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z"/>
-                </svg>
-                <div>
-                    <h2 class="font-bold">Vote Limit</h2>
-                    <p>Participants can only select X option.</p>
-                    {#if settings.voteLimit}
-                        <input bind:value={settings.voteLimitAmount} type="number" min="1"
-                               max={$currentPoll.options.length - 1}
-                               class="w-full rounded-md peer mx-auto pl-5 pr-2 py-2 border-2 border-gray-200 placeholder-gray-300"/>
-                    {/if}
-                </div>
-                <Checkbox disabled={settings.treeOptions} bind:checked={settings.voteLimit}
-                          primaryColor="#f7be00" class="my-auto ml-5"/>
-            </div>
-            <div class="grid justify-end grid-cols-3 gap-4 py-4">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
-                     class="ml-auto mr-5 my-auto" viewBox="0 0 16 16">
-                    <path d="M2 1.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1-.5-.5zm2.5.5v1a3.5 3.5 0 0 0 1.989 3.158c.533.256 1.011.791 1.011 1.491v.702c0 .7-.478 1.235-1.011 1.491A3.5 3.5 0 0 0 4.5 13v1h7v-1a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351v-.702c0-.7.478-1.235 1.011-1.491A3.5 3.5 0 0 0 11.5 3V2h-7z"/>
-                </svg>
-                <div>
-                    <h2 class="font-bold">Set deadline</h2>
-                    <p>Set a deadline to receive votes in time. After the deadline, no more votes is allowed.</p>
-                    {#if settings.deadline}
-                        <DatePicker start={dayjs().add(1, "day")} bind:selected={$currentPoll.deadline} time={false}
-                                    range={false}/>
-                    {/if}
-                </div>
-                <Checkbox bind:checked={settings.deadline}
-                          primaryColor="#f7be00" class="my-auto ml-5"/>
-            </div>
-        </div>
+  <div class="flex my-2">
+    <Checkbox
+      size="2rem"
+      disabled={settings.treeOptions}
+      bind:checked={settings.fcfs}
+      primaryColor="#f7be00"
+      class="ml-2 mb-auto"
+    />
+    <div>
+      <div class="pl-2">
+        <p class="font-bold text-left">Limit the number of votes per option</p>
+        <p class="font-light text-left">
+          First come, first served. Once the spots are filled, the option is no longer available.
+        </p>
+      </div>
     </div>
-    <div class="px-6 pt-4 pb-2">
-        <button type="submit"
-                class="hover:bg-gray-700 mb-5 bg-gray-900 text-white px-3 py-2 rounded-md ml-auto text-sm font-medium"
-                aria-current="page">Continue
-        </button>
+  </div>
+
+  <div class="flex my-2">
+    <Checkbox
+      size="2rem"
+      disabled={settings.treeOptions}
+      bind:checked={settings.voteLimit}
+      primaryColor="#f7be00"
+      class="ml-2 mb-auto"
+    />
+    <div>
+      <div class="pl-2">
+        <p class="font-bold text-left">Limit the number of votes per option</p>
+        <p class="font-light text-left">
+          First come, first served. Once the spots are filled, the option is no longer available.
+        </p>
+      </div>
+      <div class="text-left mt-1">
+        {#if settings.voteLimit}
+          <input
+            bind:value={settings.voteLimitAmount}
+            type="number"
+            min="1"
+            max={$currentPoll.options.length - 1}
+            class="w-3/4 rounded-md mx-auto border-2 border-gray-200 placeholder-gray-300"
+          />
+        {/if}
+      </div>
     </div>
+  </div>
+
+  <div class="flex my-2">
+    <Checkbox size="2rem" bind:checked={settings.deadline} primaryColor="#f7be00" class="ml-2 mb-auto" />
+    <div>
+      <div class="pl-2">
+        <p class="font-bold text-left">Set a deadline</p>
+        <p class="font-light text-left">
+          Set a deadline to receive votes in time. After the deadline, no more votes is allowed.
+        </p>
+      </div>
+      <div class="text-left mt-1">
+        {#if settings.deadline}
+          <DatePicker start={dayjs().add(1, 'day')} bind:selected={$currentPoll.deadline} time={false} range={false} />
+        {/if}
+      </div>
+    </div>
+  </div>
+  <button
+    type="submit"
+    class="hover:bg-gray-700 mb-5 bg-gray-900 text-white px-3 py-2 rounded-md ml-auto text-sm font-medium"
+    aria-current="page"
+    >Continue
+  </button>
 </form>
